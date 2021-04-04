3787ac2d1fc6c0aa5b1383fbbcf9a9b012a2c315
xwmtp <xwillmtp@gmail.com>
2021-04-04 16:20:06 +0200

    move categories_names to json file, add support for extension categories

--- Bot/Commands/Speedrun_com/Categories_info.py
+++ Bot/Commands/Speedrun_com/Categories_info.py
@@ -3,13 +3,14 @@ from Bot.Utils import readjson
 from dataclasses import dataclass
 from typing import List
 import logging
+import json
 
 GAME_IDs = {
     "oot": "j1l9qz1g",
-    # "ootextras" : "76rkv4d8"
+    "ootextras" : "76rkv4d8"
 }
 
-def download_category_info():
+def download_src_category_info():
     logging.info("Downloading categories info...")
     categories = []
     for game, game_id in GAME_IDs.items():
@@ -24,12 +25,12 @@ def download_category_info():
                         id=category_data['id'],
                         game_id=game_id,
                         weblink=category_data['weblink'],
-                        vars=_extract_variables_info(category_data['variables'])
+                        vars=_extract_src_variables_info(category_data['variables'])
                     ))
     logging.info("Completed downloading categories info.")
     return categories
 
-def _extract_variables_info(vars_data):
+def _extract_src_variables_info(vars_data):
     variables = []
     if not 'data' in vars_data or vars_data['data'] == []:
         return variables
@@ -52,6 +53,9 @@ class Variable:
     id: str
     group_id: str
     default: bool
+
+    def to_dict(self):
+        return {"id" : self.id, "name" : self.name, "nicknames" : [""]}
 
 
 @dataclass
@@ -77,3 +81,9 @@ class Category:
     def get_leaderboard(self, var_name=''):
         var = self.get_var(var_name)
         return download_leaderboard(self, var)
+
+    def to_json_string(self):
+        dict = {"name" : self.name, "id" : self.id, "nicknames" : [""]}
+        if len(self.vars) > 0:
+            dict["vars"] = [var.to_dict() for var in self.vars]
+        return json.dumps(dict, indent=2)
